51ae35ad6146bbcecb8e8820b0235b2170ede0f9
Rafael <rafael.melges456@gmail.com>
2026-01-27 02:02:36 -0300

    subindo a modware no CORS

--- src/main/java/br/com/modware/transrv/config/SecurityConfig.java
+++ src/main/java/br/com/modware/transrv/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // Angular dev
+        configuration.setAllowedOrigins(List.of("http://localhost:4200","https://modware.com.br")); // Angular dev
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
